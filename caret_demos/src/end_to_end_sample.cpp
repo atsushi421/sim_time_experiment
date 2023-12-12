f7a35cd436c5b136a3e2231039a4e6bafa2cf71d
atsushi421 <atsushi.yano.2@tier4.jp>
2023-12-12 10:59:15 +0900

    fix

--- caret_demos/src/end_to_end_sample.cpp
+++ caret_demos/src/end_to_end_sample.cpp
@@ -1,10 +1,12 @@
 #include <chrono>
 #include <memory>
+#include <deque>
 #include <vector>
+#include <fstream>
 #include <random>
 
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/int64.hpp"
 
 #define QOS_HISTORY_SIZE 10
 
@@ -18,13 +20,16 @@ public:
     int period_ms)
   : Node(node_name)
   {
-    pub_ = create_publisher<sensor_msgs::msg::Image>(pub_topic_name, QOS_HISTORY_SIZE);
-    sub_ = create_subscription<sensor_msgs::msg::Image>(
+    pub_ = create_publisher<std_msgs::msg::Int64>(pub_topic_name, QOS_HISTORY_SIZE);
+    sub_ = create_subscription<std_msgs::msg::Int64>(
       sub_topic_name, QOS_HISTORY_SIZE,
-      [&](sensor_msgs::msg::Image::UniquePtr msg)
+      [&](std_msgs::msg::Int64::UniquePtr msg)
       {
         rclcpp::sleep_for(std::chrono::milliseconds(45));
-        msg_ = std::move(msg);
+        received_msgs_.push_back(msg->data);
+        if (received_msgs_.size() > 10) {
+          received_msgs_.pop_front();
+        }
       });
 
 
@@ -32,17 +37,29 @@ public:
       std::chrono::milliseconds(period_ms), [&]()
       {
         rclcpp::sleep_for(std::chrono::milliseconds(45));
-        if (msg_) {
-          pub_->publish(*msg_);
+        RCLCPP_INFO(this->get_logger(), "Count: '%ld'", count_++);
+        while (!received_msgs_.empty()) {
+            int64_t msg = received_msgs_.front();
+            RCLCPP_INFO(this->get_logger(), "Received message: '%ld'", msg);
+            received_msgs_.pop_front();
         }
       });
+
+      log_file_.open("output/1.0/sub_timer_node_log.txt", std::ios::out | std::ios::app);
+  }
+
+  ~TimerDependencyNode()
+  {
+    log_file_.close();
   }
 
 private:
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_;
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_;
-  sensor_msgs::msg::Image::UniquePtr msg_;
+  rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr pub_;
+  rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr sub_;
+  std::deque<int64_t> received_msgs_;
   rclcpp::TimerBase::SharedPtr timer_;
+  std::ofstream log_file_;
+  int64_t count_;
 };
 
 class SubDependencyNode : public rclcpp::Node
@@ -50,22 +67,13 @@ class SubDependencyNode : public rclcpp::Node
 public:
   SubDependencyNode(
     std::string node_name,
-    std::string sub_topic_name,
     std::string subsequent_sub_topic_name,
     std::string pub_topic_name
   )
   : Node(node_name)
   {
-    sub1_ = create_subscription<sensor_msgs::msg::Image>(
-      sub_topic_name, QOS_HISTORY_SIZE, [&](sensor_msgs::msg::Image::UniquePtr msg)
-      {
-        auto msg_tmp = std::make_unique<sensor_msgs::msg::Image>();
-        msg_tmp->header.stamp = msg->header.stamp;
-        rclcpp::sleep_for(std::chrono::milliseconds(45));
-        msg_ = std::move(msg);
-      });
-    sub2_ = create_subscription<sensor_msgs::msg::Image>(
-      subsequent_sub_topic_name, QOS_HISTORY_SIZE, [&](sensor_msgs::msg::Image::UniquePtr msg)
+    sub = create_subscription<std_msgs::msg::Int64>(
+      subsequent_sub_topic_name, QOS_HISTORY_SIZE, [&](std_msgs::msg::Int64::UniquePtr msg)
       {
         (void)msg;
         rclcpp::sleep_for(std::chrono::milliseconds(45));
@@ -73,14 +81,13 @@ public:
           pub_->publish(std::move(msg_));
         }
       });
-    pub_ = create_publisher<sensor_msgs::msg::Image>(pub_topic_name, QOS_HISTORY_SIZE);
+    pub_ = create_publisher<std_msgs::msg::Int64>(pub_topic_name, QOS_HISTORY_SIZE);
   }
 
 private:
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_;
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub1_;
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub2_;
-  sensor_msgs::msg::Image::UniquePtr msg_;
+  rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr pub_;
+  rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr sub;
+  std_msgs::msg::Int64::UniquePtr msg_;
 };
 
 class SensorDummy : public rclcpp::Node
@@ -100,17 +107,16 @@ public:
     auto period = std::chrono::milliseconds(period_ms);
 
     auto callback = [&]() {
-        auto msg = std::make_unique<sensor_msgs::msg::Image>();
+        auto msg = std::make_unique<std_msgs::msg::Int64>();
         rclcpp::sleep_for(std::chrono::milliseconds(50));
-        msg->header.stamp = now();
         pub_->publish(std::move(msg));
       };
-    pub_ = create_publisher<sensor_msgs::msg::Image>(topic_name, QOS_HISTORY_SIZE);
+    pub_ = create_publisher<std_msgs::msg::Int64>(topic_name, QOS_HISTORY_SIZE);
     timer_ = create_wall_timer(period, callback);
   }
 
 private:
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_;
+  rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr pub_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
@@ -122,11 +128,11 @@ int main(int argc, char * argv[])
 
   std::vector<std::shared_ptr<rclcpp::Node>> nodes;
 
+  nodes.emplace_back(std::make_shared<SensorDummy>("drive_node", "/drive", 3000));
   nodes.emplace_back(
-    std::make_shared<SubDependencyNode>("message_driven_node", "/topic2", "/drive", "/topic3"));
+    std::make_shared<SubDependencyNode>("message_driven_node", "/drive", "/topic3"));
   nodes.emplace_back(
-    std::make_shared<TimerDependencyNode>("timer_driven_node", "/topic3", "/topic4", 100)); // 10Hz
-  nodes.emplace_back(std::make_shared<SensorDummy>("drive_node", "/drive", 100)); // 10Hz
+    std::make_shared<TimerDependencyNode>("timer_driven_node", "/topic3", "/topic4", 10000));
 
   for (auto & node : nodes) {
     executor->add_node(node);
